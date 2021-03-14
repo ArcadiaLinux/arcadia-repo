762a5c14140fffc2873e6df888fcacf1b150f60c
Nishal Kulkarni <nishalkulkarni@gmail.com>
2021-03-14 14:46:43 +0530

    removed siging flag

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -4,9 +4,8 @@ rm arcadia_repo*
 
 
 echo "repo-add"
-repo-add -s -n -R arcadia_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R arcadia_repo.db.tar.gz *.pkg.tar.zst
 sleep 5
-#cp arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
 
 echo "####################################"
 echo "Repo Updated!!"
